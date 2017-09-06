2a6d1b2f088f9a701820ac3f871f412a3077fecc
Jessica <jdhpabst@gmail.com>
2017-09-06 12:58:54 -0600

    working on both

--- arraysPractice.js
+++ arraysPractice.js
@@ -243,7 +243,17 @@ Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 
   //Code Here
 
-
+  function both(arr1, arr2){
+    let answer = [];
+    for(var i = 0; i < arr1.length; i++){
+      for(var j = 0; j < arr2.length; j++){
+        if(arr1[i] === arr2[j]){
+          answer.push(arr1[i]);
+        }
+      }
+    }
+    return answer;
+  }
 
 
 //NEXT PROBLEM
