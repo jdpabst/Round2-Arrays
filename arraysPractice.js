e67e7d9c48be5b2469bc607794147236f8844c2e
Jessica <jdhpabst@gmail.com>
2017-09-06 13:02:54 -0600

    finished devmtnemployee prob

--- arraysPractice.js
+++ arraysPractice.js
@@ -292,12 +292,18 @@ array with those four objects. After that console.log the length of the Array an
 sure that it's equal to 4. */
 
   //Code Here
+    devMountainEmployees.push(tyler, cahlan, ryan, colt);
+    console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
   //Code Here
-
+    for(var i = 0; i < devMountainEmployees.length; i++){
+      if(devMountainEmployees[i].name === "Cahlan"){
+        devMountainEmployees.splice(i, 1);
+      }
+    }
 
 
 
